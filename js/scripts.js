1592c9c288f86723cf62bda321d96b4a7cfff5ad
nasstyasever <nasstya.sever@gmail.com>
2023-06-11 21:03:33 +0200

    new

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout:2000,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:2
+            },
+            1100:{
+                items:5
+            }
+        }
+    });
