15722c6c6b14dc2422a751b0814ab7d4f3e6faa4
nasstyasever <nasstya.sever@gmail.com>
2023-07-04 03:24:01 +0200

    new

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,10 @@ $(document).ready(function(){
             0: {
                 items: 1
             },
-            1100: {
+            1000: {
+                items: 2
+            },
+            1064: {
                 items: 3
             }
         }
